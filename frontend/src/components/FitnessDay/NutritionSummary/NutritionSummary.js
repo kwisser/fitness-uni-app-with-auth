71c55a44d67a20c2c4cc7d4489034d9da661e610
Klemens Wisser <klemens.wisser@cloudwuerdig.com>
2023-06-17 13:34:01 +0200

    changed design for nutrient charts

--- frontend/src/components/FitnessDay/NutritionSummary/NutritionSummary.js
+++ frontend/src/components/FitnessDay/NutritionSummary/NutritionSummary.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Typography, Box, styled, useTheme } from '@mui/material';
+import { Typography, Grid, styled, useTheme } from '@mui/material';
 import CaloriesPieChart from '../FitnessDayActivities/CaloriesPieChart';
 
-const Container = styled('div')`
+const Container = styled(Grid)`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.values.md}px) {
-    flex-direction: row;
-  }
+  align-items: center;
 `;
 
 const NutritionSummary = ({
@@ -33,38 +30,48 @@ const NutritionSummary = ({
 
     return (
         <Container theme={theme}>
-            <Box>
-                <Typography variant="body1">Sie benötigen heute {caloriesNeeded} Kalorien.</Typography>
-                <Typography variant="body1">Bisher gegessene {caloriesReached} Kalorien.</Typography>
-                <Typography variant="body1">Kalorien {balanceText(caloriesBalance)} kcal</Typography>
-                <div style={{ width: '200px', height: '200px' }}>
-                    <CaloriesPieChart key={caloriesReached} consumed={caloriesReached} total={caloriesNeeded} />
-                </div>
-            </Box>
-            <Box>
-                <Typography variant="body1">Protein benötigt: {proteinNeeded.toFixed(2)}g</Typography>
-                <Typography variant="body1">Protein heute bisher gegessen: {proteinReached.toFixed(2)}g</Typography>
-                <Typography variant="body1">Protein {balanceText(proteinBalance)} g</Typography>
-                <div style={{ width: '200px', height: '200px' }}>
-                    <CaloriesPieChart key={proteinReached} consumed={proteinReached} total={proteinNeeded} />
-                </div>
-            </Box>
-            <Box>
-                <Typography variant="body1">Fett benötigt: {fatNeeded.toFixed(2)}g</Typography>
-                <Typography variant="body1">Fett heute bisher gegessen: {fatReached.toFixed(2)}g</Typography>
-                <Typography variant="body1">Fett {balanceText(fatBalance)} g</Typography>
-                <div style={{ width: '200px', height: '200px' }}>
-                    <CaloriesPieChart key={fatReached} consumed={fatReached} total={fatNeeded} />
-                </div>
-            </Box>
-            <Box>
-                <Typography variant="body1">Kohlenhydrate benötigt: {carbsNeeded.toFixed(2)}g</Typography>
-                <Typography variant="body1">Kohlenhydrate heute bisher gegessen: {carbsReached.toFixed(2)}g</Typography>
-                <Typography variant="body1">Kohlenhydrate {balanceText(carbsBalance)} g</Typography>
-                <div style={{ width: '200px', height: '200px' }}>
-                    <CaloriesPieChart key={carbsReached} consumed={carbsReached} total={carbsNeeded} />
-                </div>
-            </Box>
+            <Grid container alignItems="center" justifyContent="center" spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <Typography variant="h6">Sie benötigen heute {caloriesNeeded} Kalorien.</Typography>
+                    <Typography variant="body2">Bisher gegessene {caloriesReached} Kalorien.</Typography>
+                    <Typography variant="body2">Kalorien {balanceText(caloriesBalance)} kcal</Typography>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <div style={{ width: '200px', height: '200px' }}>
+                        <CaloriesPieChart key={caloriesReached} consumed={caloriesReached} total={caloriesNeeded} />
+                    </div>
+                </Grid>
+
+                {/* Protein section */}
+                <Grid item xs={12} sm={4}>
+                    <Typography variant="body2">Protein benötigt: {proteinNeeded.toFixed(2)}g</Typography>
+                    <Typography variant="body2">Protein heute bisher gegessen: {proteinReached.toFixed(2)}g</Typography>
+                    <Typography variant="body2">Protein {balanceText(proteinBalance)} g</Typography>
+                    <div style={{ width: '200px', height: '200px' }}>
+                        <CaloriesPieChart key={proteinReached} consumed={proteinReached} total={proteinNeeded} />
+                    </div>
+                </Grid>
+
+                {/* Fat section */}
+                <Grid item xs={12} sm={4}>
+                    <Typography variant="body2">Fett benötigt: {fatNeeded.toFixed(2)}g</Typography>
+                    <Typography variant="body2">Fett heute bisher gegessen: {fatReached.toFixed(2)}g</Typography>
+                    <Typography variant="body2">Fett {balanceText(fatBalance)} g</Typography>
+                    <div style={{ width: '200px', height: '200px' }}>
+                        <CaloriesPieChart key={fatReached} consumed={fatReached} total={fatNeeded} />
+                    </div>
+                </Grid>
+
+                {/* Carbs section */}
+                <Grid item xs={12} sm={4}>
+                    <Typography variant="body2">Kohlenhydrate benötigt: {carbsNeeded.toFixed(2)}g</Typography>
+                    <Typography variant="body2">Kohlenhydrate heute bisher gegessen: {carbsReached.toFixed(2)}g</Typography>
+                    <Typography variant="body2">Kohlenhydrate {balanceText(carbsBalance)} g</Typography>
+                    <div style={{ width: '200px', height: '200px' }}>
+                        <CaloriesPieChart key={carbsReached} consumed={carbsReached} total={carbsNeeded} />
+                    </div>
+                </Grid>
+            </Grid>
         </Container>
     );
 };
